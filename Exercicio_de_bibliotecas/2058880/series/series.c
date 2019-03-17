1a568a8c1082a9d5500507816539b58c0ee077f7
Andrey Naligatski Dias <andreydias@alunos.utfpr.edu.br>
2019-03-17 18:53:23 -0300

    Exercicio de bibliotecas

--- Exercicio_de_bibliotecas/2058880/series/series.c
+++ Exercicio_de_bibliotecas/2058880/series/series.c
@@ -10,6 +10,14 @@ int sequencia_pow(int n) {
 }
 
 //II. 1 – 2 + 3 – 4 + 5 ...... n, n inteiro positivo
-int sequencia_alternando(int a) {
-
+int sequencia_alternando(int n) {
+  //A soma segue um padrão, onde números ímpares são somados e pares subtraídos
+  int i = 0, soma = 0;
+  for (i = 1; i <= n; i++) {//Laço para somar n vezes
+    if (i % 2 == 0) {//Caso o número em questão for par, subtrai este
+      soma -= i;
+    }
+    else soma += i;//Caso contrário, se for ímpar, soma este
+  }
+  return soma;
 }
