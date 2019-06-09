3f1a270bf901efbb151f11fa7e48e900f6631821
Andrey Naligatski Dias <43612323+Dreyyyyy@users.noreply.github.com>
2019-06-09 14:52:59 -0300

    Biblioteca de listas duplamente encadeadas

---
+++ Lista_de_exercicios_7/lib.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+* Faça uma biblioteca para as definições
+* abaixo especificadas.
+*/
+typedef int TipoChave;
+typedef int TipoValor;
+struct TipoListaEncadeada
+{
+ TipoChave chave;
+ TipoValor valorQualquer;
+ struct TipoListaEncadeada *prox;
+ struct TipoListaEncadeada *ant;
+};
+typedef struct TipoListaEncadeada TipoListaEncadeada;
+
+TipoListaEncadeada *aloca_no(int chave, int valor);
+
+void printa_lista(TipoListaEncadeada *lista);
+
+TipoListaEncadeada *insereInicioListaEncadeada(TipoListaEncadeada **prim, TipoChave chave, TipoValor valor);
+
+TipoListaEncadeada *insereFimListaEncadeada(TipoListaEncadeada **prim, TipoChave chave, TipoValor valor);
+
+int removeNo(TipoListaEncadeada **prim, TipoChave chave);
+
+TipoListaEncadeada *copiaLista3(TipoListaEncadeada *prim);
